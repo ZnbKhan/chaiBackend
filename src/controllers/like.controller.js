fd6223cb6dfe662338b4c02dc28a32fa2e09260a
zainab khan <zainabkhanamu121@gmail.com>
2025-06-17 04:36:52 +0530

    like controller done and dusted

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import {ApiError} from "../utils/ApiErrors.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { Video } from "../models/video.models.js"
+import { Tweet } from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     //TODO: toggle like on video
@@ -49,19 +50,86 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
     //TODO: toggle like on comment
+    // got commentId and userId from req.params and req.user._id
+    // check comment exist
+    // if comment is liked then unlike
+    // otherwise like and sabe
+
+    const {commentId} = req.params;
+    const userId = req.user._id;
+
+    if(!commentId){
+        throw new ApiError(400, "commentId is missing");
+    }
+
+    const comment =  await Comment.findById(commentId);
+    if(!comment){
+        throw new ApiError(400, "Comment not found")
+    }
+
+    const existingComment = await Like.findOne({comment:commentId})
+
+    if(existingComment){
+        await existingComment.deleteOne();
+        return res.status(200).json(new ApiResponse(200, {}, "Comment unliked Sucessfully"));
+    }
+
+    const newComment = await Like.create({
+        likedBy:userId,
+        comment:commentId,
+    })
+
+    return res.status(200).json(new ApiResponse(200, newComment, "liked comment sucesfully"))
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+    
+    const {tweetId} = req.params
+    const userId = req.user._id;
+
+    if(!tweetId){
+        throw new ApiError(400, "Tweet Id not found");
+    }
+
+     const tweet =  await Tweet.findById(tweetId)
+     console.log(tweet)
+     if(!tweet){
+        throw new ApiError(400, "Tweet Not found")
+     }
+
+     const existingTweetLike = await Like.findOne({
+        tweet:tweetId,
+        likedBy:userId
+     })
+
+     if(existingTweetLike){
+        await existingTweetLike.deleteOne();
+        return res.status(200).json(new ApiResponse(200, "unlike tweet sucessfully"))
+     }
+
+     const newTweetLike = await Like.create({
+        tweet:tweetId,
+        likedBy:userId,
+     })
+
+     return res.status(200).json(new ApiResponse(200, newTweetLike, "Tweet Liked Sucesfully"))
+ })
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    // user is already loggedin get his/her all liked video
+    // userId se populate kr lenge video bhi
+
+    const userId = req.user._id;
+
+    const likedVideo = await Like.find({likedBy:userId}).populate("video")
+    // console.log(likedVideo)
+    return res.status(200).json(new ApiResponse(200, likedVideo, "Fetched all liked Video Sucessfully"))
+
+
 })
 
 export {
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.route("/video/:videoId").post(verifyJWT, toggleVideoLike)
+router.route("/comment/:commentId").post(verifyJWT, toggleCommentLike)
+router.route("/tweet/:tweetId").post(verifyJWT, toggleTweetLike)
+router.route("/liked-video").get(verifyJWT, getLikedVideos)
 
 export default router;
